ca923f34b84f53dae3d2a05c9166e799f633d3e5
Jared Patterson <Jaredp75@hotmil.com>
2017-11-13 15:46:20 -0500

    Contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+	export default class Contact extends Component {
+	  render(){
+	    return(
+	      <div className="contact-wrapper">
+	        <h1>Contact Me</h1>
+	        <div className="contact-items">
+
+	          <a href="mailto:jaredheel75@gmail.com"><i className="fa fa-envelope fa-5x" aria-hidden="true"></i> </a>
+	        </div>
+	      </div>
+	    )
+	  }
+	}
