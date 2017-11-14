95b1484f9189f4c4c16a2b3f7477c04262a70071
Jared Patterson <Jaredp75@hotmil.com>
2017-11-14 10:54:51 -0500

    App.js and index.js updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Navbar>
+      <Switch>
+        <Route exact path = "/portfolio" component={About}/>
+        <Route path = "/projects" component={Projects}/>
+        <Route path = "/contact" component={Contact}/>
+      </Switch>
+    </Navbar>
+  </BrowserRouter>
+
+
+
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
