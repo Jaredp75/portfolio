a811d10157a6b456fa33d486aa6ff368d97e60fe
Jared Patterson <Jaredp75@hotmil.com>
2017-11-14 11:14:54 -0500

    index.js update

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
     <Navbar>
       <Switch>
         <Route exact path = "/" component={About}/>
+        <Route path = "/about" component={About}/>
         <Route path = "/projects" component={Projects}/>
         <Route path = "/contact" component={Contact}/>
       </Switch>
